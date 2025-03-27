5a11cdef89ce108f2c8d30b32d8f9a1931abf591
mubarak <luismubarak@gmail.com>
2025-03-27 22:45:10 +0200

    release version 0.0.1

--- database/factories/RolesFactory.php
+++ database/factories/RolesFactory.php
@@ -17,7 +17,7 @@ class RolesFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title'=>'admin'
         ];
     }
 }
--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Roles;
+use Database\Factories\RolesFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,6 @@ class RolesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Roles::factor(1)->create();
     }
 }
